4985d687f31744706ba7fb8b4bada3e474987113
James Hudson <jmh357@comcast.net>
2022-11-06 15:55:10 -0600

    create Team model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Team(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  constructors_championships = models.IntegerField()
+  drivers_championships = models.IntegerField()
+  race_wins = models.IntegerField()
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-class Team:
-  def __init__(self, name, location, constructors_championships, drivers_championships, race_wins):
-    self.name = name
-    self.location = location
-    self.constructors_championships = constructors_championships
-    self.drivers_championships = drivers_championships
-    self.race_wins = race_wins
+# class Team:
+#   def __init__(self, name, location, constructors_championships, drivers_championships, race_wins):
+#     self.name = name
+#     self.location = location
+#     self.constructors_championships = constructors_championships
+#     self.drivers_championships = drivers_championships
+#     self.race_wins = race_wins
 
-teams = [
-  Team('Red Bull Racing', 'Milton Keynes, England, UK', 5, 6, 91),
-  Team('Mercedes-AMG Petronas F1 Team', 'Brackley, England, UK', 8, 9, 124),
-  Team('Scuderia Ferrari', 'Maranello, Province of Modena, Italy', 16, 15, 241),
-  Team('Mclaren F1 Team', 'Mclaren Technology Centre, Woking, Surrey, England, UK', 8, 12, 183)
-]
+# teams = [
+#   Team('Red Bull Racing', 'Milton Keynes, England, UK', 5, 6, 91),
+#   Team('Mercedes-AMG Petronas F1 Team', 'Brackley, England, UK', 8, 9, 124),
+#   Team('Scuderia Ferrari', 'Maranello, Province of Modena, Italy', 16, 15, 241),
+#   Team('Mclaren F1 Team', 'Mclaren Technology Centre, Woking, Surrey, England, UK', 8, 12, 183)
+# ]
 
 def home(request):
   return render(request, 'home.html')
