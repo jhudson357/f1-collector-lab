1040d6949f7e6b57473a8a581fede8f901afc3fa
James Hudson <jmh357@comcast.net>
2022-11-06 16:17:02 -0600

    add admin access to Team model

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from .models import Team
 
 # Create your views here.
-
-# teams = [
-#   Team('Red Bull Racing', 'Milton Keynes, England, UK', 5, 6, 91),
-#   Team('Mercedes-AMG Petronas F1 Team', 'Brackley, England, UK', 8, 9, 124),
-#   Team('Scuderia Ferrari', 'Maranello, Province of Modena, Italy', 16, 15, 241),
-#   Team('Mclaren F1 Team', 'Mclaren Technology Centre, Woking, Surrey, England, UK', 8, 12, 183)
-# ]
-
 def home(request):
   return render(request, 'home.html')
 
